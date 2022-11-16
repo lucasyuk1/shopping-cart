309dfe27214e7bf87f6a219d3050c4326e3de759
LucasCS <lcsvdk@gmail.com>
2022-11-16 18:29:43 -0300

    Requisitos 1 e 2

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -1,7 +1,16 @@
+const fetchProductEndpoint = (endpoint) => fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${endpoint}`);
+
 export const fetchProduct = () => {
   // seu código aqui
 };
 
-export const fetchProductsList = () => {
-  // seu código aqui
+export const fetchProductsList = async (p) => {
+  if (!param) throw new Error('Termo de busca não informado');
+  try {
+    const response = await fetchProductEndpoint(p);
+    const data = await response.json();
+    return data.results;
+  } catch {
+    return false;
+  }
 };
