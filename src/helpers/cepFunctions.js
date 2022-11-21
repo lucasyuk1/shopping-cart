067b86d7e792db17a2e54c4e4a5925497bca3304
LucasCS <lcsvdk@gmail.com>
2022-11-21 15:33:40 -0300

    Requisições da API do CEP implementadas e projeto finalizado

--- src/helpers/cepFunctions.js
+++ src/helpers/cepFunctions.js
@@ -1,7 +1,31 @@
-export const getAddress = () => {
-  // seu código aqui
+const iCep = document.querySelector('.cep-input');
+const cartAddress = document.querySelector('.cart__address');
+
+export const getAddress = async (cep) => {
+  const error404 = 404;
+  const req1 = fetch(`https://cep.awesomeapi.com.br/json/${cep}`);
+  const req2 = fetch(`https://brasilapi.com.br/api/cep/v2/${cep}`);
+  const address = await Promise.any([req1, req2])
+    .then((resp) => {
+      if (resp.status === error404) {
+        throw new Error('CEP não encontrado');
+      }
+      return resp.json();
+    })
+    .then((data) => data)
+    .catch(() => { throw new Error('CEP não encontrado'); });
+  return address;
 };
 
-export const searchCep = () => {
-  // seu código aqui
+export const searchCep = async () => {
+  try {
+    const cep = iCep.value;
+    const data = await getAddress(cep);
+    const { street, address, neighborhood, district,
+      city, state } = data;
+    const res = `${street || address} - ${neighborhood || district} - ${city} - ${state}`;
+    cartAddress.innerHTML = res;
+  } catch (e) {
+    cartAddress.innerText = e.message;
+  }
 };
